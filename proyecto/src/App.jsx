55e32e0a5fee15c2c8580c9d93fa3d4090e008a8
FredBrave <freddy.bravo.robalino@gmail.com>
2026-01-08 11:59:54 -0500

    feat: listado de modalidades completado

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ModalidadesPage } from './pages/ModalidadesPage';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ModalidadesPage from './pages/ModalidadesPage';
+
 function App() {
   return (
     <BrowserRouter>
+      <nav className="navbar navbar-dark bg-dark px-3">
+        <Link className="navbar-brand" to="/modalidades">Modalidades</Link>
+      </nav>
+
       <Routes>
         <Route path="/modalidades" element={<ModalidadesPage />} />
       </Routes>
--- proyecto/src/pages/ModalidadesPage.jsx
+++ proyecto/src/pages/ModalidadesPage.jsx
@@ -1,5 +1,42 @@
-export function ModalidadesPage() {
+import { useEffect, useState } from "react";
+import api from "../api/api";
+
+
+export default function ModalidadesPage() {
+    const [modalidades, setModalidades] = useState([]);
+
+    const cargarModalidades = async () => {
+        const res = await api.get("modalidades/");
+        setModalidades(res.data);
+    }
+    useEffect(() => {
+        cargarModalidades();
+
+    }, []);
+
     return (
-        <div>ModalidadesPage</div>
-    )
+        <div className="container mt-4">
+            <h2>Modalidades</h2>
+
+
+            <table className="table table-bordered mt-3">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {modalidades.map((m) => (
+                        <tr key={m.id}>
+                            <td>{m.id}</td>
+                            <td>{m.nombre}</td>
+                            <td>{m.estado ? "Activo" : "Inactivo"}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
